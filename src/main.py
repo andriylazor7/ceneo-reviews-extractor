e3cb4c42715698e7d4b2af04bb9cd4c983a9ca5d
AndriyLazor <236327@student.uek.krakow.pl>
2025-03-11 20:57:09 +0100

    Modified main script to extract all opinions from a single page

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
-from scraper import fetch_html, extract_single_opinion
+from scraper import fetch_html, extract_all_opinions
 
 if __name__ == "__main__":
     product_id = input("Enter product ID: ")
     html = fetch_html(product_id)
 
     if html:
-        opinion = extract_single_opinion(html)
-        print(opinion)
+        opinions = extract_all_opinions(html)
+        for opinion in opinions:
+          print(opinion)
