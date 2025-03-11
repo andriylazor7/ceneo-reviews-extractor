154c2e493551282efe1eeeb16d4c52fd36d4b882
AndriyLazor <236327@student.uek.krakow.pl>
2025-03-11 22:22:03 +0100

    Integrated JSON and CSV saving into main script

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
-from scraper import extract_all_pages
+from scraper import extract_all_pages, save_to_json, save_to_csv
 
 if __name__ == "__main__":
     product_id = input("Enter product ID: ")
     opinions = extract_all_pages(product_id)
 
     print(f"Total opinions extracted: {len(opinions)}")
-    for opinion in opinions:
-      print(opinion)
+
+    if opinions:
+        save_to_json(opinions, product_id)
+
+        save_to_csv(opinions, product_id)
